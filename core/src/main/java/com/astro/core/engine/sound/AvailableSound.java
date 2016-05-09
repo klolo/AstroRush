60a7d98d1ed2ffd5665c2a429a5b6f67ba596f56
Kamil Lolo <kamil.lolo@mail.com>
2016-05-09 22:22:06 +0200

    jacoco plugin

---
+++ core/src/main/java/com/astro/core/engine/sound/AvailableSound.java
@@ -0,0 +1,9 @@
+package com.astro.core.engine.sound;
+
+/**
+ * Created by kamil on 09.05.16.
+ */
+public enum AvailableSound {
+
+    SOUND_1
+}
---
+++ core/src/main/java/com/astro/core/engine/sound/SoundManager.java
@@ -0,0 +1,16 @@
+package com.astro.core.engine.sound;
+
+/**
+ * Created by kamil on 09.05.16.
+ */
+public enum SoundManager {
+
+    instance;
+
+
+    public void playSound(AvailableSound sound) {
+
+    }
+
+
+}
